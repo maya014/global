1c786818ca132beb255d626b3c867c9d8054f441
მაია კვარაცხელია <74621259+maya014@users.noreply.github.com>
2021-08-18 23:15:09 +0400

    0.1

---
+++ js/script.js
@@ -0,0 +1,66 @@
+let currentPage = 1;
+let totalPages;
+
+function getUsers(page) {
+    function render() {
+        let response = this.responseText;
+        let responseData = JSON.parse(response);
+
+        var fragment = document.createDocumentFragment();
+        
+        responseData.data.forEach(item => {
+            let li = document.createElement('li');
+            li.classList.add('li-users');
+
+            let span = document.createElement('span');
+            span.textContent = item.first_name;
+
+            let img = document.createElement('img');
+            img.src = item.avatar;
+            img.classList.add('image-wraper');
+
+            li.appendChild(img);
+            li.appendChild(span);
+
+            fragment.appendChild(li);
+        });
+
+        document.getElementById('list').innerHTML = ' ';
+        document.getElementById('list').appendChild(fragment);
+
+        totalPages = responseData.total_pages;
+        
+    }
+
+
+    let requist = new XMLHttpRequest();
+    requist.addEventListener('load',render);
+    // requist.addEventListener('error', errorFunction);
+    requist.open('GET', 'https://reqres.in/api/users?page=' + page);
+
+    requist.send(); 
+}
+
+
+document.getElementById('previous').addEventListener('click', function() {
+    if (currentPage === 1) {
+        return;
+    }
+
+    currentPage -=1;
+    getUsers(currentPage);
+});
+
+
+document.getElementById('next').addEventListener('click', function() {
+    if (currentPage === totalPages) {
+        return;
+    }
+
+    currentPage +=1;
+    getUsers(currentPage);
+});
+
+
+
+getUsers(currentPage);
