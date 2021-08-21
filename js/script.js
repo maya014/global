07752e899e076f44d157c9a7a23be42143b8b503
მაია კვარაცხელია <74621259+maya014@users.noreply.github.com>
2021-08-21 22:41:24 +0400

    ....

--- js/script.js
+++ js/script.js
@@ -64,3 +64,16 @@ document.getElementById('next').addEventListener('click', function() {
 
 
 getUsers(currentPage);
+
+
+// .NAVNAR
+
+let navbarlinks = document.getElementById('navbarlinks');
+
+let togglebutton = document.getElementById('togglebutton');
+
+togglebutton.addEventListener('click', function()
+{
+    navbarlinks.classList.toggle('active')
+})
+
